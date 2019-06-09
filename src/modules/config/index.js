37d56b6515e4a3caab6c093013abc4bb2f72dd09
uki00a <uki00a@gmail.com>
2019-06-10 00:22:08 +0900

    update: read settings from XDG_CONFIG_HOME

--- src/modules/config/index.js
+++ src/modules/config/index.js
@@ -1,18 +1,28 @@
 import * as fs from 'fs';
 import * as path from 'path';
-import * as os from 'os';
+import xdgBaseDir from 'xdg-basedir';
 
+// TODO improve error handling
 export function readSettings() {
-  return new Promise((resolve, reject) => {
-    // TODO read settings from `~/.config/chatwork-term/settings.json`
-    const path2config = path.join(os.homedir(), '.chatwork-term.json');
-    fs.readFile(path2config, (error, data) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(JSON.parse(data));
-      }
-    });
-  });
+  return readJSONFile(buildConfigPath('settings.json'));
 }
 
+function buildConfigPath(fileName) {
+  return path.join(xdgBaseDir.config, 'chatwork-term', fileName);
+}
+
+function readJSONFile(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, (error, data) => {
+      if (error) {
+        return reject(error);
+      }
+
+      try {
+        return resolve(JSON.parse(data));
+      } catch (error) {
+        return reject(error);
+      }
+    })
+  });
+}
