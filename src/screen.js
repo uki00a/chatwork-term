c1528548e8bae3002cb668d2fbee15a6ea770ab8
uki00a <uki00a@gmail.com>
2019-06-16 20:42:44 +0900

    refactor: move focusNext() out of createScreen()

--- src/screen.js
+++ src/screen.js
@@ -18,17 +18,18 @@ export default function createScreen() {
   });
   screen.program.disableMouse();
   
-  function focusNext(screen) {
-    const isInputElement = typeof screen.focused.cancel === 'function';
-    if (isInputElement) {
-      const nextIndex = screen.children.indexOf(screen.focused) + 1;
-      const nextElement = screen.children[nextIndex];
-      screen.focused.setValue(removeLastChar(screen.focused.getValue()));
-      screen.focused.cancel();
-      screen.focusPush(nextElement);
-    } else {
-      screen.focusNext();
-    }
-  }
   return screen;
+}
+
+function focusNext(screen) {
+  const isInputElement = typeof screen.focused.cancel === 'function';
+  if (isInputElement) {
+    const nextIndex = screen.children.indexOf(screen.focused) + 1;
+    const nextElement = screen.children[nextIndex];
+    screen.focused.setValue(removeLastChar(screen.focused.getValue()));
+    screen.focused.cancel();
+    screen.focusPush(nextElement);
+  } else {
+    screen.focusNext();
+  }
 }
