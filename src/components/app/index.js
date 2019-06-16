2380f6bbfe1cb747cc4d9f7ca77b75bf1ab42752
uki00a <uki00a@gmail.com>
2019-06-16 20:08:45 +0900

    fix: bug about posting a message

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -117,7 +117,7 @@ export default function App({
         })
       }
     ]));
-  }, [messageEditor]);
+  }, [messageEditor, addMessageToRoom]);
 
   const activateMessagePreviewer = useCallback(() => {
     dispatch(actions.activeShortcutsChanged([
